5cc49e0776eeea8f27504057e5969370cd6d41c7
Jenny Lai <jlai902@gmail.com>
2016-11-24 05:15:42 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,71 @@
 # Write your code here.
+# "hello" becomes 'hi'
+# "to, two, too" become '2'
+# "for, four" become '4'
+# 'be' becomes 'b'
+# 'you' becomes 'u'
+# "at" becomes "@"
+# "and" becomes "&"
+# First, write a method, dictionary, that contains the words-to-be-substituted and their substitutes. The original words
+# from the list below will be the keys. The substitute of a particular word becomes that key's value. You will use this
+# hash in later methods to look up words and replace them with their substitutes.
+def dictionary
+  substitutes = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  substitutes = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+  tweet.split(" ").collect do |word|
+    if substitutes.has_key? (word.downcase)
+      word = substitutes[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    tweet = word_substituter(tweet)
+    puts "#{tweet}"
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet = selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    tweet[0..136] + "..."
+  else
+    tweet
+  end
+end
